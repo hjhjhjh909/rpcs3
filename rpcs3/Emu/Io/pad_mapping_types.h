4967d4e87248bc6db13c0443ec83d5d541c73438
hjhjhjh909 <pffracing99@gmail.com>
2025-05-20 10:36:03 +0100

    Update pad_mapping_types.h

--- rpcs3/Emu/Io/pad_mapping_types.h
+++ rpcs3/Emu/Io/pad_mapping_types.h
@@ -1,1 +1,16 @@
- 
+ // rpcs3/Emu/Io/pad_mapping_types.h
+#pragma once
+
+enum class sdl_mapping_type {
+    button,
+    axis,
+    hat
+};
+
+enum class hat_component {
+    none,
+    up,
+    down,
+    left,
+    right
+};

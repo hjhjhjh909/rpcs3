1e94b274c94751a465435c0bc40502585589ed5f
hjhjhjh909 <pffracing99@gmail.com>
2025-05-20 09:10:47 +0100

    Update ThrustmasterT500RSConfig.cpp

--- rpcs3/Emu/Io/ThrustmasterT500RSConfig.cpp
+++ rpcs3/Emu/Io/ThrustmasterT500RSConfig.cpp
@@ -5,7 +5,6 @@
 #include "Utilities/File.h"
 #include "ThrustmasterT500RSConfig.h"
 
-
 LOG_CHANNEL(cfg_log, "CFG");
 
 emulated_t500rs_config g_cfg_t500rs;
@@ -17,35 +16,41 @@ emulated_t500rs_config::emulated_t500rs_config()
 
 void emulated_t500rs_config::reset()
 {
-    from_default();
+    const std::lock_guard lock(m_mutex);
+    cfg::node::from_default();
 }
 
 void emulated_t500rs_config::save(bool lock_mutex)
 {
+    std::unique_lock lock(m_mutex, std::defer_lock);
     if (lock_mutex)
-        std::lock_guard lock(m_mutex);
+    {
+        lock.lock();
+    }
+    cfg_log.notice("Saving ThrustmasterT500RS config: '%s'", m_path);
 
-    cfg_log.notice("Saving T500RS config: '%s'", m_path);
+    if (!fs::create_path(fs::get_parent_dir(m_path)))
+    {
+        cfg_log.fatal("Failed to create path: '%s' (%s)", m_path, fs::g_tls_error);
+    }
 
     if (!cfg::node::save(m_path))
     {
-        cfg_log.error("Failed to save T500RS config to '%s' (error=%s)", m_path, fs::g_tls_error);
+        cfg_log.error("Failed to save ThrustmasterT500RS config to '%s' (error=%s)", m_path, fs::g_tls_error);
     }
 }
 
 bool emulated_t500rs_config::load()
 {
-    std::lock_guard lock(m_mutex);
+    const std::lock_guard lock(m_mutex);
 
-    cfg_log.notice("Loading T500RS config: %s", m_path);
+    cfg_log.notice("Loading ThrustmasterT500RS config: %s", m_path);
 
     from_default();
 
     if (fs::file cfg_file{m_path, fs::read})
     {
-        std::string content;
-        content.resize(cfg_file.size());
-        if (cfg_file.read(content.data(), content.size()) == content.size())
+        if (const std::string content = cfg_file.to_string(); !content.empty())
         {
             return from_string(content);
         }
@@ -57,3 +62,5 @@ bool emulated_t500rs_config::load()
 
     return true;
 }
+
+#endif
